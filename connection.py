0d90ed7adfe1b7b38563fbb479cc02e23210081d
Adna Lakisic <adnalakisic@gmail.com>
2022-05-15 15:04:39 -0400

    Removed other py file as it was too big

---
+++ connection.py
@@ -0,0 +1,29 @@
+#Libraries
+import csv
+import MySQLdb 
+import sys
+
+#Connecting to a MySQL database
+
+class MySQLConnection:
+    con = MySQLdb.connect(host='mariadb2.mostardesigns.com', 
+                          port=3306, 
+                          user='root',
+                          database='nyc_restaurants',
+                          passwd='fl!ntst0n3')
+    c = con.cursor()
+
+    # Get the count of customers_dataset table
+    c.execute(''' SELECT count(DBA) FROM DOHMH_New_York_City_Restaurant_Inspection_Results ''')
+    # If the count is 1, then customers_dataset table already exists
+    row = c.fetchone()
+    if row != None : {
+        print('Table already created.')
+    }
+    else: 
+        # Read all files from csv to db format
+        c.execute('''LOAD DATA INFILE '/var/lib/mysql/DOHMH_New_York_City_Restaurant_Inspection_Results.csv'
+    INTO TABLE `DOHMH_New_York_City_Restaurant_Inspection_Results`
+    FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+    IGNORE 1 ROWS;''')
+        print('Table created.')
